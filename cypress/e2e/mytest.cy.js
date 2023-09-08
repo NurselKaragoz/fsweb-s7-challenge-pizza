74025f3bb553ff3fabe909af335a3921e8c31340
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-09-08 16:45:50 +0300

    build edildi

---
+++ cypress/e2e/mytest.cy.js
@@ -0,0 +1,9 @@
+// describe("Order"),
+//   () => {
+//     beforeEach(() => {
+//       cy.visit("http://localhost:3000/");
+//     });
+//     it("Pizza Order sayasına gidiyor mu?", () =>
+
+//     cy.get(".order-pizza").click().should)};
+//   };
