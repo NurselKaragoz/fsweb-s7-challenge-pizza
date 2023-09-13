9d9d85e83e17c1ca72ec70ce615902733edb0b1a
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-09-13 16:07:41 +0300

    build edildi

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./companents/home";
 import Order from "./companents/pizza";
 import Onay from "./companents/onay";
+const [orderData, setOrderData] = useState();
 
 const App = () => {
   return (
     <>
       <Switch>
         <Route path="/pizza" exact component={Order} />
-        <Route path="/" exact component={Home} />
+        {/* <Route path="/" exact component={Home} /> */}
         <Route path="/succes" exact component={Onay} />
+        <Route path="/"></Route>
       </Switch>
     </>
   );
--- src/companents/onay.js
+++ src/companents/onay.js
@@ -2,14 +2,6 @@ import React, { useEffect } from "react";
 import "./onaypage.css";
 import axios from "axios";
 
-axios
-  .get("https://reqres.in/api/users?delay=3")
-  .then((res) => {
-    console.log("success=>", res);
-    console.log("deneme", res.data.value);
-  })
-  .catch((err) => console.log(err.response));
-
 const Onay = () => {
   return (
     <>
@@ -19,7 +11,6 @@ const Onay = () => {
         </h1>
         <h2> TEBRİKLER!</h2>
         <h2> SİPARİŞİNİZ ALINDI</h2>
-        <div className="ordered-what">Sipariş Detayı </div>
       </div>
     </>
   );
